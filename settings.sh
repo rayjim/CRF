06567be62ec8e4ad3cfcd7d22455b537bcb83008
Rayjim <baoruihan@gmail.com>
2012-03-18 15:54:33 +0900

    from air

--- settings.sh
+++ settings.sh
@@ -1,2 +1,4 @@
-# settings for CRF..
-export CLASSPATH=$CRF_HOME/lib/CRF.jar:$CRF_HOME/lib/colt.jar:$CRF_HOME/lib/LBFGS.jar:$CLASSPATH
+# settings for CRF.
+CRF_HOME="/Users/Ray/Documents/projects/CRF" 
+export CLASSPATH=$CRF_HOME/lib/CRF.jar:$CRF_HOME/lib/colt.jar:$CRF_HOME/lib/LBFGS.jar:$CRF_HOME/lib/trove.jar
+echo $CLASSPATH
