67b5d6edac25e08e26eeb27731b20023ed4a222b
Ray Bao <baoruihan@gmail.com>
2012-03-19 10:27:15 +0900

    from Pro

--- settings.sh
+++
@@ -1,4 +0,0 @@
-# settings for CRF.
-CRF_HOME="/Users/Ray/Documents/projects/CRF" 
-export CLASSPATH=$CRF_HOME/lib/CRF.jar:$CRF_HOME/lib/colt.jar:$CRF_HOME/lib/LBFGS.jar:$CRF_HOME/lib/trove.jar
-echo $CLASSPATH
